f66275baf2d83cdac63bf02aa6781827f3852a16
Ashish Surve <ashish.surve@outlook.in>
2021-01-07 13:19:14 +0530

    port

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ async def get_image(style: str, file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=environ.get('PORT'))
